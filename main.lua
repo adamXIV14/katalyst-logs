101bdd23ed74491f730f41e0f059a7bb57325ade
adamXIV14 <adamayat910@gmail.com>
2026-08-09 19:22:09 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 {
     "users": {
-        "4982886890": "ez"
+        "id": "reason"
     },
     "keys": {
-        "KAT4-1786731940-4982886890-2618d02b0547d876": "blacklisted, say sorry on general"
+        "key": "reason"
     }
 }
